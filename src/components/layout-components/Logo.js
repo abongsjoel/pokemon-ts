6c7e42f0d82f32decad391cd6badb9399f6dc09d
Chi Joel <abongsjoel@gmail.com>
2022-02-20 17:25:47 +0100

    Use Pokemon for logo

--- src/components/layout-components/Logo.js
+++ src/components/layout-components/Logo.js
@@ -1,65 +1,74 @@
-import React from 'react'
-import { SIDE_NAV_WIDTH, SIDE_NAV_COLLAPSED_WIDTH, NAV_TYPE_TOP } from 'constants/ThemeConstant';
-import { APP_NAME } from 'configs/AppConfig';
+import React from "react";
+import {
+  SIDE_NAV_WIDTH,
+  SIDE_NAV_COLLAPSED_WIDTH,
+  NAV_TYPE_TOP,
+} from "constants/ThemeConstant";
+import { APP_NAME } from "configs/AppConfig";
 import { connect } from "react-redux";
-import utils from 'utils';
-import { Grid } from 'antd';
+import utils from "utils";
+import { Grid, Typography } from "antd";
 
 const { useBreakpoint } = Grid;
+const { Title, Text } = Typography;
 
 const getLogoWidthGutter = (props, isMobile) => {
   const { navCollapsed, navType } = props;
-  const isNavTop = navType === NAV_TYPE_TOP ? true : false
-  if(isMobile && !props.mobileLogo) {
-    return 0
+  const isNavTop = navType === NAV_TYPE_TOP ? true : false;
+  if (isMobile && !props.mobileLogo) {
+    return 0;
   }
-  if(isNavTop) {
-    return 'auto'
+  if (isNavTop) {
+    return "auto";
   }
-  if(navCollapsed) {
-    return `${SIDE_NAV_COLLAPSED_WIDTH}px`
+  if (navCollapsed) {
+    return `${SIDE_NAV_COLLAPSED_WIDTH}px`;
   } else {
-    return `${SIDE_NAV_WIDTH}px`
+    return `${SIDE_NAV_WIDTH}px`;
   }
-}
+};
 
 const getLogo = (props) => {
   const { navCollapsed, logoType } = props;
-  if(logoType === 'light') {
-    if(navCollapsed) {
-      return '/img/logo-sm-white.png'
+  if (logoType === "light") {
+    if (navCollapsed) {
+      return "/img/logo-sm-white.png";
     }
-    return '/img/logo-white.png'
+    return "/img/logo-white.png";
   }
 
   if (navCollapsed) {
-    return '/img/logo-sm.png'
+    return "/img/logo-sm.png";
   }
-  return '/img/logo.png'
-}
+  return "/img/logo.png";
+};
 
 const getLogoDisplay = (isMobile, mobileLogo) => {
-  if(isMobile && !mobileLogo) {
-    return 'd-none'
+  if (isMobile && !mobileLogo) {
+    return "d-none";
   } else {
-    return 'logo'
+    return "logo";
   }
-}
+};
 
 export const Logo = (props) => {
-  const isMobile = !utils.getBreakPoint(useBreakpoint()).includes('lg')
+  const isMobile = !utils.getBreakPoint(useBreakpoint()).includes("lg");
   return (
     <div
-      className={getLogoDisplay(isMobile, props.mobileLogo)} 
-      style={{width: `${getLogoWidthGutter(props, isMobile)}`}}>
-      <img src={getLogo(props)} alt={`${APP_NAME} logo`}/>
+      className={getLogoDisplay(isMobile, props.mobileLogo)}
+      style={{ width: `${getLogoWidthGutter(props, isMobile)}` }}
+    >
+      {/* <img src={getLogo(props)} alt={`${APP_NAME} logo`} /> */}
+      <Text style={{ fontSize: "2em" }} strong>
+        Pokemon
+      </Text>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = ({ theme }) => {
-  const { navCollapsed, navType } =  theme;
-  return { navCollapsed, navType }
+  const { navCollapsed, navType } = theme;
+  return { navCollapsed, navType };
 };
 
 export default connect(mapStateToProps)(Logo);
